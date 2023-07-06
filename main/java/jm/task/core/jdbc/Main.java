3bad975aa2b7f3da3479e28932310290342ba19b
Shellder55 <thetwillfeed@gmail.com>
2023-07-06 17:12:20 +0600

    mySQL1

---
+++ main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,29 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        // реализуйте алгоритм здесь
+
+//        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//        userDaoJDBC.createUsersTable();
+
+//        userDaoJDBC.saveUser("Vitaliy", "Usoltsev", (byte) 20);
+//        userDaoJDBC.saveUser("Evgeniy", "Usoltsev", (byte) 31);
+//        userDaoJDBC.saveUser("Irina", "Pamorzina", (byte) 21);
+//        userDaoJDBC.saveUser("Alexandr", "Feredyk", (byte) 42);
+
+//        userDaoJDBC.getAllUsers();
+
+//        userDaoJDBC.removeUserById(4);
+
+//        userDaoJDBC.cleanUsersTable();
+
+//        userDaoJDBC.dropUsersTable();
+
+    }
+}
