86f3f590d3a6e257b2004f78d1452c06816c3523
Shellder55 <thetwillfeed@gmail.com>
2023-07-09 21:11:53 +0600

    java v2

---
+++ CoreTaskTemplate-master v2/src/main/java/jm/task/core/jdbc/dao/UserDao.java
@@ -0,0 +1,21 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserDao {
+    void createUsersTable() throws SQLException, ClassNotFoundException;
+
+    void dropUsersTable() throws ClassNotFoundException, SQLException;
+
+    void saveUser(String name, String lastName, byte age) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException;
+
+    void removeUserById(long id) throws SQLException, ClassNotFoundException;
+
+    List<User> getAllUsers() throws ClassNotFoundException, SQLException;
+
+    void cleanUsersTable() throws ClassNotFoundException, SQLException;
+}
---
+++ CoreTaskTemplate-master v2/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,47 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static final String databaseUrl = "jdbc:mysql://localhost:3306/mySQL_DB?serverTimezone=UTC";
+    private static final String hostName = "localhost";
+    private static final String userName = "admin";
+    private static final String password = "1812rtif";
+    private static final String timeZone = "?serverTimezone=UTC";
+    public static final String nameDB = "Humans";
+
+//    public Connection connection = DriverManager.getConnection(databaseUrl, userName, password);
+//    public Statement statement = connection.createStatement();
+
+    private static Connection connection = null;
+    private static Statement statement = null;
+
+    public static Connection getConnection() throws SQLException {
+        connection = DriverManager.getConnection(databaseUrl, userName, password);
+        return Util.connection;
+    }
+
+    public static Statement getStatement() throws SQLException {
+        statement = connection.createStatement();
+        return Util.statement;
+    }
+
+
+    public Util() throws SQLException {
+    }
+
+    public static Connection getMySQLConnection()
+            throws SQLException {
+        return getMySQLConnection(hostName, nameDB, userName, password, timeZone);
+    }
+
+    public static Connection getMySQLConnection(String hostName, String nameDB, String userName, String password, String timeZone)
+            throws SQLException {
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + nameDB + timeZone;
+        return DriverManager.getConnection(connectionURL, userName, password);
+    }
+}
